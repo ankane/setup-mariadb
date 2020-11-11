664d31088e35c64311a3e4201957b60a788225b4
Andrew Kane <andrew@chartkick.com>
2020-11-11 00:56:53 -0800

    Test without key

--- index.js
+++ index.js
@@ -23,10 +23,8 @@ if (process.platform == 'darwin') {
   run(`echo "${bin}" >> $GITHUB_PATH`);
 } else {
   // install
-  run(`sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8`);
+  // run(`sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8`);
   run(`echo "deb http://downloads.mariadb.com/MariaDB/mariadb-${mariadbVersion}/repo/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) main" | sudo tee /etc/apt/sources.list.d/mariadb.list`);
-  // run(`curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadbVersion}" --skip-maxscale --skip-tools`);
-  // run(`cat /etc/apt/sources.list.d/mariadb.list`);
   run(`sudo apt-get update`);
   run(`sudo apt-get install mariadb-server-${mariadbVersion}`);
 
