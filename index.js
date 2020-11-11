03e3e38e7dedacf48720cfe55fbcd2e46723f0a3
Andrew Kane <andrew@chartkick.com>
2020-11-11 00:50:33 -0800

    Try script

--- index.js
+++ index.js
@@ -23,10 +23,12 @@ if (process.platform == 'darwin') {
   run(`echo "${bin}" >> $GITHUB_PATH`);
 } else {
   // install
-  run(`sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8`);
-  run(`sudo add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/${mariadbVersion}/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) main"`)
-  run(`sudo apt-get update`);
-  run(`sudo apt-get install mariadb-server-${mariadbVersion}`);
+  // run(`sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8`);
+  // run(`sudo add-apt-repository "deb [arch=amd64,arm64,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/${mariadbVersion}/ubuntu $(. /etc/os-release && echo $VERSION_CODENAME) main"`)
+  // run(`sudo apt-get update`);
+  // run(`sudo apt-get install mariadb-server-${mariadbVersion}`);
+  run(`curl -LsS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash -s -- --mariadb-server-version="mariadb-${mariadbVersion}" --skip-maxscale --skip-tools`);
+  run(`cat /etc/apt/sources.list.d/mariadb.list`);
 
   // start
   run(`sudo systemctl start mariadb`);
